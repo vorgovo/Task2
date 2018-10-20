7768153f26a16d3b00a572e8d2bb9136b4c21606
vorgovo <32040628+vorgovo@users.noreply.github.com>
2018-10-20 19:52:28 +0300

    update

---
+++ code/CMakeFiles/big_integer_testing.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/work/ITMO/Sorokin2/Task2-3/code/big_integer.cpp" "C:/work/ITMO/Sorokin2/Task2-3/code/CMakeFiles/big_integer_testing.dir/big_integer.cpp.obj"
+  "C:/work/ITMO/Sorokin2/Task2-3/code/big_integer_testing.cpp" "C:/work/ITMO/Sorokin2/Task2-3/code/CMakeFiles/big_integer_testing.dir/big_integer_testing.cpp.obj"
+  "C:/work/ITMO/Sorokin2/Task2-3/code/gtest/gtest-all.cc" "C:/work/ITMO/Sorokin2/Task2-3/code/CMakeFiles/big_integer_testing.dir/gtest/gtest-all.cc.obj"
+  "C:/work/ITMO/Sorokin2/Task2-3/code/gtest/gtest_main.cc" "C:/work/ITMO/Sorokin2/Task2-3/code/CMakeFiles/big_integer_testing.dir/gtest/gtest_main.cc.obj"
+  "C:/work/ITMO/Sorokin2/Task2-3/code/vector_opt.cpp" "C:/work/ITMO/Sorokin2/Task2-3/code/CMakeFiles/big_integer_testing.dir/vector_opt.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ code/CMakeFiles/big_integer_testing.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/big_integer_testing.dir/big_integer_testing.cpp.obj"
+  "CMakeFiles/big_integer_testing.dir/gtest/gtest-all.cc.obj"
+  "CMakeFiles/big_integer_testing.dir/gtest/gtest_main.cc.obj"
+  "CMakeFiles/big_integer_testing.dir/big_integer.cpp.obj"
+  "CMakeFiles/big_integer_testing.dir/vector_opt.cpp.obj"
+  "big_integer_testing.pdb"
+  "big_integer_testing.exe"
+  "big_integer_testing.exe.manifest"
+  "libbig_integer_testing.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/big_integer_testing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
